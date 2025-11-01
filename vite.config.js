ea8c5a7bc26cd70497c83040009c183212eeeaf5
Machahary07 <jeumachahary07@gmail.com>
2025-11-01 23:02:08 +0530

    jsons

--- vite.config.js
+++ vite.config.js
@@ -9,19 +9,22 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
-    // increase limit if you want to avoid the warning
     chunkSizeWarningLimit: 1000,
-    // simple manualChunks to split node_modules
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'vendor_react';
-            if (id.includes('gsap') || id.includes('matter-js') || id.includes('lucide-react')) return 'vendor_ui';
-            return 'vendor';
+            if (id.includes('react')) return 'vendor_react'
+            if (
+              id.includes('gsap') ||
+              id.includes('matter-js') ||
+              id.includes('lucide-react')
+            )
+              return 'vendor_ui'
+            return 'vendor'
           }
-        }
-      }
-    }
-  }
+        },
+      },
+    },
+  },
 })
